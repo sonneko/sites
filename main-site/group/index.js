bb4732abcfc8887a61837318ed5e0a2beaba164d
sonneko <sonneko27@gmail.com>
2024-07-30 17:00:32 +0900

    z

--- main-site/group/index.js
+++
@@ -1,9 +0,0 @@
-
-
-function GroupIndex() {
-    return (
-        <h1>This is Group Index.</h1>
-    )
-}
-
-export default GroupIndex;
--- main-site/member/index.js
+++
@@ -1,9 +0,0 @@
-
-
-function MemberIndex() {
-    return(
-        <h1>This is memberIndex</h1>
-    )
-}
-
-export default MemberIndex;
